f8c60baf1a48b1425ad9bab426fc7b499f709947
Ujval Gupta <134779004+ug-is-og@users.noreply.github.com>
2023-08-23 02:49:33 +0530

    Time: 0 ms (100.00%), Space: 39.6 MB (68.70%) - LeetHub

--- 0168-excel-sheet-column-title/0168-excel-sheet-column-title.java
+++ 0168-excel-sheet-column-title/0168-excel-sheet-column-title.java
@@ -1,4 +1,5 @@
 // A->1 se start ho ra 0 se nahi iss wajah se saara kaam complex ho ra
+// https://www.youtube.com/watch?v=X_vJDpCCuoA&t=239s
 // iss question ki approach examples se hi figure out ho payegi
 class Solution {
     public String convertToTitle(int columnNumber) {
