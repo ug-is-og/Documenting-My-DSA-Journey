d494895edec44f860bbcd75be36f3c19bdd889cc
Ujval Gupta <134779004+ug-is-og@users.noreply.github.com>
2023-08-24 11:09:15 +0530

    Time: 0 ms (100.00%), Space: 39.9 MB (81.41%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.java
+++ 0216-combination-sum-iii/0216-combination-sum-iii.java
@@ -1,36 +1,101 @@
 // Brute force approach
 
+// class Solution {
+//     public List<List<Integer>> combinationSum3(int k, int n) {
+//         List<List<Integer>> ans=new ArrayList<>();
+//         List<Integer> temp=new ArrayList<>();
+//         helper(k,n,temp,ans);
+//         return ans;
+//     }
+//     public void helper(int k,int remSum,List<Integer> temp,List<List<Integer>> ans)
+//     {
+//         if(k==0&&remSum!=0)
+//         return;
+//         if(remSum==0)
+//         {
+//           ArrayList<Integer> newTemp=null;
+//           if(k==0)
+//           {
+//             newTemp=new ArrayList<>(temp);
+//             Collections.sort(newTemp);
+//             if(!ans.contains(newTemp))
+//             ans.add(newTemp);
+//           }
+//           return;
+//         }
+//         for(int i=1;i<=9;i++)
+//         {
+//             if(remSum>=i&&!temp.contains(i))
+//             {
+//                 temp.add(i);
+//                 helper(k-1,remSum-i,temp,ans);
+//                 temp.remove(temp.size()-1);
+//             }
+//         }
+//     }
+// }
+
+
+// Better Approach
+// but is this the optimal approach, think about it
+
+// class Solution {
+//     public List<List<Integer>> combinationSum3(int k, int n) {
+//         List<List<Integer>> ans=new ArrayList<>();
+//         List<Integer> temp=new ArrayList<>();
+//         helper(1,k,n,temp,ans);
+//         return ans;
+//     }
+//     public void helper(int index,int k,int remSum,List<Integer> temp,List<List<Integer>> ans)
+//     {
+//         if(k==0&&remSum!=0)
+//         return;
+//         if(remSum==0)
+//         {
+//           ArrayList<Integer> newTemp=null;
+//           if(k==0)
+//           {
+//             newTemp=new ArrayList<>(temp);
+//             Collections.sort(newTemp);
+//             if(!ans.contains(newTemp))
+//             ans.add(newTemp);
+//           }
+//           return;
+//         }
+//         if(index>9) // isko upar likhoge toh 12th test case nahi chalega
+//         return;
+//         // no take
+//         helper(index+1,k,remSum,temp,ans);
+//         // take
+//         temp.add(index);
+//         helper(index+1,k-1,remSum-index,temp,ans);
+//         temp.remove(temp.size()-1);
+//     }
+// }
+
+// for optimal approach, ab jo bhi optimizations honi hai woh above code mein hi hongi
+// this is all
+
 class Solution {
     public List<List<Integer>> combinationSum3(int k, int n) {
         List<List<Integer>> ans=new ArrayList<>();
         List<Integer> temp=new ArrayList<>();
-        helper(k,n,temp,ans);
+        helper(1,k,n,temp,ans);
         return ans;
     }
-    public void helper(int k,int remSum,List<Integer> temp,List<List<Integer>> ans)
+    public void helper(int index,int k,int remSum,List<Integer> temp,List<List<Integer>> ans)
     {
-        if(k==0&&remSum!=0)
-        return;
-        if(remSum==0)
+        if(index>9)
         {
-          ArrayList<Integer> newTemp=null;
-          if(k==0)
-          {
-            newTemp=new ArrayList<>(temp);
-            Collections.sort(newTemp);
-            if(!ans.contains(newTemp))
-            ans.add(newTemp);
-          }
+          if(k==0&&remSum==0)
+          ans.add(new ArrayList<>(temp));
           return;
         }
-        for(int i=1;i<=9;i++)
-        {
-            if(remSum>=i&&!temp.contains(i))
-            {
-                temp.add(i);
-                helper(k-1,remSum-i,temp,ans);
-                temp.remove(temp.size()-1);
-            }
-        }
+        // no take
+        helper(index+1,k,remSum,temp,ans);
+        // take
+        temp.add(index);
+        helper(index+1,k-1,remSum-index,temp,ans);
+        temp.remove(temp.size()-1);
     }
 }
