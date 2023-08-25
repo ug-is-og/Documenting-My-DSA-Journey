b8a63b2368c288e6c16b5fd5aa268fdc0c99ed5b
Ujval Gupta <134779004+ug-is-og@users.noreply.github.com>
2023-08-26 02:11:46 +0530

    Time: 1 ms (82.75%), Space: 39.5 MB (99.97%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -1,40 +1,61 @@
 // Brute force approach
 // TC O(N*max length of string)
 // find largest string, aur ab harr ek string ka usse comparison kar, jis min point pe break ho re woh ho jayega answer
+
+// class Solution {
+//     public String longestCommonPrefix(String[] strs) {
+//         int index=0,maxLength=Integer.MIN_VALUE;
+//         // yeh edge case bhul gaya tha hence nahi chala tha code for last test case
+//         if(strs.length==1)
+//         return strs[0];
+//         for(int i=0;i<strs.length;i++)
+//         {
+//             if(maxLength<strs[i].length())
+//             {
+//                 maxLength=strs[i].length();
+//                 index=i;
+//             }
+//         }
+//         String temp=strs[index];
+//         int minIndex=Integer.MAX_VALUE;
+//         for(int i=0;i<strs.length;i++)
+//         {
+//             if(i!=index)
+//             {
+//                 String temp2=strs[i];
+//                 int j=0;
+//                 for(j=0;j<Math.min(temp2.length(),temp.length());j++)
+//                 {
+//                     if(temp.charAt(j)==temp2.charAt(j))
+//                     continue;
+//                     else
+//                     break;
+//                 }
+//                 minIndex=Math.min(minIndex,j);
+//             }
+//         }
+//          if(minIndex==Integer.MAX_VALUE)
+//          return "";
+//         return strs[0].substring(0,minIndex);
+//     }
+// }
+
+// Optimized Approach
+// TC is O(NlogN + M) M is length of minimum string
+
 class Solution {
     public String longestCommonPrefix(String[] strs) {
-        int index=0,maxLength=Integer.MIN_VALUE;
-        // yeh edge case bhul gaya tha hence nahi chala tha code for last test case
-        if(strs.length==1)
-        return strs[0];
-        for(int i=0;i<strs.length;i++)
+        Arrays.sort(strs);
+        String first=strs[0];
+        String last=strs[strs.length-1];
+        int index=0;
+        while(index<first.length())
         {
-            if(maxLength<strs[i].length())
-            {
-                maxLength=strs[i].length();
-                index=i;
-            }
+            if(first.charAt(index)==last.charAt(index))
+            index++;
+            else
+            break;
         }
-        String temp=strs[index];
-        int minIndex=Integer.MAX_VALUE;
-        for(int i=0;i<strs.length;i++)
-        {
-            if(i!=index)
-            {
-                String temp2=strs[i];
-                int j=0;
-                for(j=0;j<Math.min(temp2.length(),temp.length());j++)
-                {
-                    if(temp.charAt(j)==temp2.charAt(j))
-                    continue;
-                    else
-                    break;
-                }
-                minIndex=Math.min(minIndex,j);
-            }
-        }
-         if(minIndex==Integer.MAX_VALUE)
-         return "";
-        return strs[0].substring(0,minIndex);
+        return strs[0].substring(0,index);
     }
 }
