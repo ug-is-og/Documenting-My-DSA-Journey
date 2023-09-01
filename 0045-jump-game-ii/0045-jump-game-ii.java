d8ec300fc40acea47d29378f0d41a765a7396da1
Ujval Gupta <134779004+ug-is-og@users.noreply.github.com>
2023-09-02 00:24:52 +0530

    Time: 1 ms (99.32%), Space: 45.2 MB (6.79%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -29,14 +29,34 @@ import java.util.*;
 
 class Solution {
     public int jump(int nums[]) {
-        for (int i = 1; i < nums.length; i++) {
-            nums[i]=Math.max(nums[i] + i, nums[i-1]);
+        int ans=0,jmp=0;
+        if(nums.length==1)
+            return 0;
+        for(int i=0;i<nums.length;i++)
+        {
+            int max=Integer.MIN_VALUE;
+            int newPos=Integer.MIN_VALUE;
+            jmp++;
+            int pos=i;
+            int jmpValue=nums[i];
+            if(nums[i]==0)
+            return -1;
+            if(jmpValue+pos>=nums.length-1)
+            {
+                ans=jmp;
+                break;
+            }
+            for(int j=pos+1;j<=pos+jmpValue;j++)
+            {
+                if(max<nums[j]+j) // yeh statement bhot crucial hai dhyan se samajhna 
+                // pehle main max< nums[j] karke check kar ra tha and woh work nahi karega , chahe toh karke dekhlo
+                {
+                    max=nums[j]+j;
+                    newPos=j;
+                }
+            }
+            i=newPos-1;
         }
-        int idx = 0, c = 0;
-        while (idx < nums.length - 1) {
-            c++;
-            idx = nums[idx];
-        }
-        return c;
+        return ans;
     }
 }
