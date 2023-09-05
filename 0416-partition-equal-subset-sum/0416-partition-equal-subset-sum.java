88733c573026ba4d0b9f1b2da9a4de0d280c0613
Ujval Gupta <134779004+ug-is-og@users.noreply.github.com>
2023-09-05 13:33:57 +0530

    Time: 56 ms (48.10%), Space: 45.9 MB (69.48%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -39,6 +39,55 @@
 
 
 
+// class Solution {
+//     public boolean canPartition(int[] nums) {
+//         int sum=0;
+//         for(int i=0;i<nums.length;i++)
+//         sum=sum+nums[i];
+//         if(sum%2!=0)
+//         return false;
+//         int target=sum/2;
+//         // now it's typical subset sum problem
+//         int dp[][]=new int[target+1][nums.length];
+//         for(int temp[]:dp)
+//         Arrays.fill(temp,-1);
+//         return helper(0,target,nums,dp);
+//     }
+//     public boolean helper(int index,int k,int nums[],int dp[][])
+//     {
+//         if(k==0)
+//         return true;
+//         if(index==nums.length)
+//         return false;
+//         if(dp[k][index]!=-1)
+//         {
+//             if(dp[k][index]==1)
+//             return true;
+//             else
+//             return false;
+//         }
+//         boolean way1=false,way2=false,ans=false;
+//         if(k>=nums[index])
+//         {
+//             way1=helper(index+1,k-nums[index],nums,dp);
+//             way2=helper(index+1,k,nums,dp);
+//             ans=way1||way2;
+//         }
+//         else
+//         {
+//             ans=helper(index+1,k,nums,dp);
+//         }
+//         if(ans==true)
+//         dp[k][index]=1;
+//         else
+//         dp[k][index]=0;
+//         return ans;
+//     }
+// }
+
+// Let's try tabulation for this
+// did it by myself, great going
+
 class Solution {
     public boolean canPartition(int[] nums) {
         int sum=0;
@@ -47,40 +96,29 @@ class Solution {
         if(sum%2!=0)
         return false;
         int target=sum/2;
-        // now it's typical subset sum problem
-        int dp[][]=new int[target+1][nums.length];
-        for(int temp[]:dp)
-        Arrays.fill(temp,-1);
-        return helper(0,target,nums,dp);
-    }
-    public boolean helper(int index,int k,int nums[],int dp[][])
-    {
-        if(k==0)
-        return true;
-        if(index==nums.length)
-        return false;
-        if(dp[k][index]!=-1)
+        boolean dp[][]=new boolean[target+1][nums.length+1];
+        //initialisation
+        for(int j=0;j<=nums.length;j++)
+        dp[0][j]=true;
+        boolean ans=false,way1=false,way2=false;
+        // fill up the dp array
+        for(int i=1;i<dp.length;i++)
         {
-            if(dp[k][index]==1)
-            return true;
-            else
-            return false;
+            for(int j=1;j<dp[0].length;j++)
+            {
+                if(i>=nums[j-1])
+                {
+                    way1=dp[i-nums[j-1]][j-1];
+                    way2=dp[i][j-1];
+                    ans=way1||way2;
+                }
+                else
+                {
+                    ans=dp[i][j-1];
+                }
+                dp[i][j]=ans;
+            }
         }
-        boolean way1=false,way2=false,ans=false;
-        if(k>=nums[index])
-        {
-            way1=helper(index+1,k-nums[index],nums,dp);
-            way2=helper(index+1,k,nums,dp);
-            ans=way1||way2;
-        }
-        else
-        {
-            ans=helper(index+1,k,nums,dp);
-        }
-        if(ans==true)
-        dp[k][index]=1;
-        else
-        dp[k][index]=0;
-        return ans;
+        return dp[target][nums.length];
     }
 }
