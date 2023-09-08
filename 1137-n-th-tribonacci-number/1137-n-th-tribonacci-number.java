877c8112ca1c8e99be7dfa51f8fa474161ddfd5e
Ujval Gupta <134779004+ug-is-og@users.noreply.github.com>
2023-09-08 23:45:34 +0530

    Time: 0 ms (100.00%), Space: 39 MB (69.55%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
@@ -1,18 +1,36 @@
+// class Solution {
+//     public int tribonacci(int n) {
+//         if(n==0)
+//         return 0;
+//         if(n==1||n==2)
+//         return 1;
+//         int dp[]=new int[n+1];
+//         Arrays.fill(dp,-1);
+//         dp[0]=0;
+//         dp[1]=1;
+//         dp[2]=1;
+//         for(int i=3;i<dp.length;i++)
+//         {
+//             dp[i]=dp[i-1]+dp[i-2]+dp[i-3];
+//         }
+//         return dp[dp.length-1];
+//     }
+// }
+
 class Solution {
     public int tribonacci(int n) {
         if(n==0)
         return 0;
         if(n==1||n==2)
         return 1;
-        int dp[]=new int[n+1];
-        Arrays.fill(dp,-1);
-        dp[0]=0;
-        dp[1]=1;
-        dp[2]=1;
-        for(int i=3;i<dp.length;i++)
+        int a=0,b=1,c=1,d=0;
+        for(int i=1;i<=n-2;i++)
         {
-            dp[i]=dp[i-1]+dp[i-2]+dp[i-3];
+            d=a+b+c;
+            a=b;
+            b=c;
+            c=d;
         }
-        return dp[dp.length-1];
+        return d;
     }
 }
