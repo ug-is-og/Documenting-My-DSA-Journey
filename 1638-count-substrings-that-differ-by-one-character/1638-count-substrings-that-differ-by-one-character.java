e3df21936ad5ebb7287638e100efa7f1aa9c83fa
Ujval Gupta <134779004+ug-is-og@users.noreply.github.com>
2023-09-14 00:42:51 +0530

    Time: 10 ms (39.18%), Space: 43.2 MB (31.58%) - LeetHub

--- 1638-count-substrings-that-differ-by-one-character/1638-count-substrings-that-differ-by-one-character.java
+++ 1638-count-substrings-that-differ-by-one-character/1638-count-substrings-that-differ-by-one-character.java
@@ -1,6 +1,8 @@
 // pehle brute force approach socha ki t ke saare substrings length wise store karlo and s ke length wise substrings build karte jao and cross check with the lengthwise substrings of t ki konse aese hai jisme difference kewal 1 ka aayega 
 
-// par substrings generate karne ki complexity toh n! hai toh TLE aa jayega, fir hamne issi approach ko optimize karne ka socha using Fraz's solution
+// par substrings generate karne ki complexity toh n^2 hai toh brute force sa lag jayega, ....eventually brute approach ki complexity aayegi O(N^2)*O(m^2)*O(N) fir hamne issi approach ko optimize karne ka socha using Fraz's solution
+
+// n! is the no of substrings, it isn't the complexity
 
 // https://www.youtube.com/watch?v=EyJUpI0nowY
 
@@ -8,42 +10,78 @@
 
 // Go for the DP approach now
 
+// class Solution {
+//     public int countSubstrings(String s, String t) {
+//         int ans=0;
+//         for(int i=0;i<s.length();i++)
+//         {
+//             for(int j=0;j<t.length();j++)
+//             {
+//                 char c1=s.charAt(i);
+//                 char c2=t.charAt(j);
+//                 int diff=0,i1=i,j1=j;
+//                 while(diff<=1)
+//                 {
+//                     if(c1==c2)
+//                     {
+//                         if(diff==1)
+//                         {
+//                             ans++;
+//                         }
+//                     }
+//                     else 
+//                     {
+//                         diff++;
+//                         if(diff>1)
+//                         break;
+//                         ans++;
+//                     }
+//                     if(i1<s.length()-1&&j1<t.length()-1)
+//                     {
+//                         c1=s.charAt(++i1);
+//                         c2=t.charAt(++j1);
+//                     }
+//                     else
+//                     break;
+//                 }
+//             }
+//         }
+//         return ans;
+//     }
+// }
+
+// DP approach
+
+// refer version 2 of xieuyan 95 solution
+// refer this video as well youtube.com/watch?v=CGokZpAxzLY
+
 class Solution {
     public int countSubstrings(String s, String t) {
-        int ans=0;
-        for(int i=0;i<s.length();i++)
+        int dp[][][]=new int[s.length()+1][t.length()+1][2];
+        int sum=0;
+        // first fill the diff=0 part
+        for(int i=1;i<s.length()+1;i++)
         {
-            for(int j=0;j<t.length();j++)
+            for(int j=1;j<t.length()+1;j++)
             {
-                char c1=s.charAt(i);
-                char c2=t.charAt(j);
-                int diff=0,i1=i,j1=j;
-                while(diff<=1)
+                if(s.charAt(i-1)==t.charAt(j-1))
                 {
-                    if(c1==c2)
-                    {
-                        if(diff==1)
-                        {
-                            ans++;
-                        }
-                    }
-                    else 
-                    {
-                        diff++;
-                        if(diff>1)
-                        break;
-                        ans++;
-                    }
-                    if(i1<s.length()-1&&j1<t.length()-1)
-                    {
-                        c1=s.charAt(++i1);
-                        c2=t.charAt(++j1);
-                    }
-                    else
-                    break;
+                    dp[i][j][0]=1+dp[i-1][j-1][0];
                 }
             }
         }
-        return ans;
+        // now we will use diff=0 part while filling diff=1 part
+        for(int i=1;i<s.length()+1;i++)
+        {
+            for(int j=1;j<t.length()+1;j++)
+            {
+                if(s.charAt(i-1)!=t.charAt(j-1))
+                dp[i][j][1]=1+dp[i-1][j-1][0];
+                else
+                dp[i][j][1]=dp[i-1][j-1][1];
+                sum=sum+dp[i][j][1];
+            }
+        }
+        return sum;
     }
 }
