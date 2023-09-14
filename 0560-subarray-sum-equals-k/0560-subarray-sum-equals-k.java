7b1f6363e48abf4aedcb56b6802cb108eb1542cc
Ujval Gupta <134779004+ug-is-og@users.noreply.github.com>
2023-09-15 01:00:27 +0530

    Time: 25 ms (50.71%), Space: 46.3 MB (46.78%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -1,22 +1,50 @@
+// class Solution {
+//     public int subarraySum(int[] nums, int k) {
+//         // build the prefix array
+//         int prefix[]=new int[nums.length];
+//         prefix[0]=nums[0];
+//         for(int i=1;i<nums.length;i++)
+//         {
+//             prefix[i]=prefix[i-1]+nums[i];
+//         }
+//         int count=0;
+//         for(int i=0;i<nums.length;i++)
+//         {
+//             if(prefix[i]==k) // keep in mind
+//             count++; 
+//             for(int j=0;j<i;j++)
+//             {
+//                 if(prefix[i]-prefix[j]==k)
+//                 count++;
+//             }
+//         }
+//         return count;
+//     }
+// }
+
+// optimized solution
+
 class Solution {
     public int subarraySum(int[] nums, int k) {
-        // build the prefix array
-        int prefix[]=new int[nums.length];
-        prefix[0]=nums[0];
-        for(int i=1;i<nums.length;i++)
-        {
-            prefix[i]=prefix[i-1]+nums[i];
-        }
         int count=0;
+        int prefix[]=new int[nums.length];
+        HashMap<Integer,Integer> map=new HashMap<>();
+        map.put(0,1);
         for(int i=0;i<nums.length;i++)
         {
-            if(prefix[i]==k) // keep in mind
-            count++; 
-            for(int j=0;j<i;j++)
+            if(i==0)
             {
-                if(prefix[i]-prefix[j]==k)
-                count++;
+                prefix[0]=nums[0];
             }
+            else
+            {
+                prefix[i]=prefix[i-1]+nums[i];
+            }
+            if(map.containsKey(prefix[i]-k))
+            count=count+map.get(prefix[i]-k);
+            map.put(prefix[i],map.getOrDefault(prefix[i],0)+1);
+            // if(map.containsKey(prefix[i]-k)) // khaas dhyan rakhna iss cheez ka considering TC : [1], 0 
+            // count++;
         }
         return count;
     }
