5493364f11302af9313d0516fb08275172a229d1
Ujval Gupta <134779004+ug-is-og@users.noreply.github.com>
2023-09-23 22:13:39 +0530

    Added solution - LeetHub

---
+++ Equilibrium Point - GFG/equilibrium-point.java
@@ -0,0 +1,61 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+
+class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t =
+            Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+        while (t-- > 0) {
+            
+            //taking input n
+            int n = Integer.parseInt(br.readLine().trim());
+            long arr[] = new long[n];
+            String inputLine[] = br.readLine().trim().split(" ");
+            
+            //adding elements to the array
+            for (int i = 0; i < n; i++) {
+                arr[i] = Long.parseLong(inputLine[i]);
+            }
+
+            Solution ob = new Solution();
+            
+            //calling equilibriumPoint() function
+            System.out.println(ob.equilibriumPoint(arr, n));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+
+    
+    // a: input array
+    // n: size of array
+    // Function to find equilibrium point in the array.
+    public static int equilibriumPoint(long arr[], int n) {
+        long preSum[]=new long[n];
+        preSum[0]=arr[0];
+        long leftSum=0,rightSum=0;
+        for(int i=1;i<arr.length;i++)
+        {
+            preSum[i]=preSum[i-1]+arr[i];
+        }
+        for(int i=0;i<n;i++)
+        {
+            if(i-1<0)
+            leftSum=0;
+            else
+            leftSum=preSum[i-1];
+            rightSum=preSum[n-1]-preSum[i];
+            if(leftSum==rightSum)
+            return i+1;
+        }
+        return -1;
+    }
+}
