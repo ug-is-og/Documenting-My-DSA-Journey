80c7ebd2dd7bd6cdae4156f96172f737052aebef
Ujval Gupta <134779004+ug-is-og@users.noreply.github.com>
2023-10-07 18:53:02 +0530

    Added solution - LeetHub

---
+++ Reverse alternate nodes in Link List - GFG/reverse-alternate-nodes-in-link-list.java
@@ -0,0 +1,123 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.lang.*;
+
+class Node
+{
+    int data;
+    Node next;
+    Node(int key)
+    {
+        data = key;
+        next = null;
+    }
+}
+
+class Rearr
+{
+    static Node head;
+    
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t  =sc.nextInt();
+        
+        while(t-- > 0)
+        {
+            int n = sc.nextInt();
+            int a1 = sc.nextInt();
+            Node head = new Node(a1);
+            Node temp = head;
+            for(int i = 1; i < n; i++)
+            {
+                int a = sc.nextInt();
+                temp.next = new Node(a);
+                temp = temp.next;
+            }
+            
+            Solution ob = new Solution();
+            ob.rearrange(head);
+            printLast(head);
+            System.out.println();
+        }
+    }
+    
+    public static void printLast(Node node)
+    {
+        while(node != null)
+        {
+            System.out.print(node.data + " ");
+            node = node.next;
+        }
+    }
+}
+// } Driver Code Ends
+
+
+/*node class of the linked list
+class Node
+{
+    int data;
+    Node next;
+    Node(int key)
+    {
+        data = key;
+        next = null;
+    }
+}*/
+
+//It is mandatory to not use recursion and do it in O(1) extra soace
+class Solution
+{
+    public static void rearrange(Node odd)
+    {
+        // build list1 and list2 as separate lists initially
+        if(odd==null||odd.next==null||odd.next.next==null)
+        {
+            return;
+        }
+        Node list1=odd,list2=odd.next;
+        Node tail1=odd,tail2=odd.next;
+        Node temp1=odd,temp2=odd.next;
+        while(temp1.next!=null&&temp2.next!=null)
+        {
+            temp1=temp1.next.next;
+            temp2=temp2.next.next;
+            tail1.next=temp1;
+            tail1=temp1;
+            tail2.next=temp2;
+            tail2=temp2;
+        }
+        // if(tail1!=null)
+        tail1.next=null;
+        if(tail2!=null)
+        tail2.next=null;
+        // Now you have to reverse the list2
+        list2=reverseLL(list2);
+        // temp1=list1;
+        // while(temp1.next!=null)
+        // {
+        //     temp1=temp1.next;
+        // }
+        tail1.next=list2;
+        odd=list1;
+    }
+    public static Node reverseLL(Node list2)
+    {
+        if(list2==null||list2.next==null)
+        {
+            return list2;
+        }
+        Node prev=list2;
+        Node curr=list2.next;
+        Node temp=list2;
+        prev.next=null;
+        while(temp!=null)
+        {
+            temp=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=temp;
+        }
+        return prev;
+    }
+}
