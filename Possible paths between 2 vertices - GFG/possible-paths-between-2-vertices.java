602cddf3816f0fc9335c3c2a63252711d59a97f2
Ujval Gupta <134779004+ug-is-og@users.noreply.github.com>
2023-10-09 23:44:39 +0530

    Added solution - LeetHub

---
+++ Possible paths between 2 vertices - GFG/possible-paths-between-2-vertices.java
@@ -0,0 +1,64 @@
+//{ Driver Code Starts
+// Initial Template for Java
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while (T-- > 0) {
+            String[] s = br.readLine().trim().split(" ");
+            int V = Integer.parseInt(s[0]);
+            int E = Integer.parseInt(s[1]);
+            ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+            for (int i = 0; i < V; i++) adj.add(i, new ArrayList<Integer>());
+            for (int i = 0; i < E; i++) {
+                String[] S = br.readLine().trim().split(" ");
+                int u = Integer.parseInt(S[0]);
+                int v = Integer.parseInt(S[1]);
+                adj.get(u).add(v);
+                // adj.get(v).add(u);
+            }
+            String[] S1 = br.readLine().trim().split(" ");
+            int source = Integer.parseInt(S1[0]);
+            int destination = Integer.parseInt(S1[1]);
+            Solution obj = new Solution();
+            System.out.println(obj.countPaths(V, adj, source, destination));
+        }
+    }
+}
+
+// } Driver Code Ends
+
+// Let's use DFS in this question
+
+class Solution {
+    // Function to count paths between two vertices in a directed graph.
+    public int countPaths(int V, ArrayList<ArrayList<Integer>> adj, int source,
+                   int destination) {
+       boolean visited[]=new boolean[V];
+       int a[]=new int[1];
+       helper(adj,source,destination,a,visited);
+       return a[0];
+    }
+    public void helper(ArrayList<ArrayList<Integer>> adj,int source,int destination,int a[],boolean visited[])
+    {
+        if(source==destination)
+        {
+            a[0]++;
+            return;
+        }
+        if(visited[source]==true)
+        {
+            return;
+        }
+        visited[source]=true;
+        for(int i=0;i<adj.get(source).size();i++)
+        {
+            helper(adj,adj.get(source).get(i),destination,a,visited);
+            visited[adj.get(source).get(i)]=false;
+        }
+    }
+}
