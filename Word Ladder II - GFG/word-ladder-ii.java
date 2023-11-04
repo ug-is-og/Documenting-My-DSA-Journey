d0fee30ce9edb53f3b32975a539ed735710c0483
Ujval Gupta <134779004+ug-is-og@users.noreply.github.com>
2023-11-04 13:29:13 +0530

    Added solution - LeetHub

--- Word Ladder II - GFG/word-ladder-ii.java
+++ Word Ladder II - GFG/word-ladder-ii.java
@@ -58,75 +58,111 @@ public class GFG
 //User function Template for Java
 
 
+
+//User function Template for Java
+
 class Solution
 {
     public ArrayList<ArrayList<String>> findSequences(String startWord, String targetWord, String[] wordList)
     {
-        Set < String > st = new HashSet < String > ();
-        int len = wordList.length;
-        for (int i = 0; i < len; i++) {
-            st.add(wordList[i]);
+        String beginWord=startWord;
+        String endWord=targetWord;
+        Queue<ArrayList<String>> q=new LinkedList<>();
+        ArrayList<String> list=new ArrayList<>();
+        ArrayList<ArrayList<String>> ans=new ArrayList<>();
+        list.add(beginWord);
+        int prevLevel=0;
+        int min=Integer.MAX_VALUE;
+        Set<String> set=new HashSet<>();
+        for(int i=0;i<wordList.length;i++)
+        {
+            set.add(wordList[i]);
         }
-
-        // Creating a queue ds which stores the words in a sequence which is
-        // required to reach the targetWord after successive transformations.
-        Queue < ArrayList < String >> q = new LinkedList < > ();
-        ArrayList < String > ls = new ArrayList < > ();
-        ls.add(startWord);
-        q.add(ls);
-        ArrayList < String > usedOnLevel = new ArrayList < > ();
-        usedOnLevel.add(startWord);
-        int level = 0;
-
-        // A vector to store the resultant transformation sequence.
-        ArrayList < ArrayList < String >> ans = new ArrayList < > ();
-        int cnt = 0;
-
-        // BFS traversal with pushing the new formed sequence in queue 
-        // when after a transformation, a word is found in wordList.
-        while (!q.isEmpty()) {
-            cnt++;
-            ArrayList < String > vec = q.peek();
-            q.remove();
-
-             // Now, erase all words that have been
-             // used in the previous levels to transform
-             if (vec.size() > level) {
-                level++;
-                for (String it: usedOnLevel) {
-                    st.remove(it);
+        set.remove(beginWord);
+        q.add(list);
+        ArrayList<String> delete=new ArrayList<>();
+        while(!q.isEmpty())
+        {
+            ArrayList<String> temp=q.remove();
+            // System.out.println(temp+" "+prevLevel+" "+temp.size());
+            ArrayList<String> newList=null;
+            String stringData=temp.get(temp.size()-1);
+            
+            
+            if(temp.size()>prevLevel)
+            {
+                for(int i=0;i<delete.size();i++)
+                {
+                    set.remove(delete.get(i));
+                }
+                prevLevel++;
+            }
+            
+            // not so special code snippet
+            
+            // if (temp.size() > prevLevel) {
+            //     prevLevel++;
+            //     for (String it: delete) {
+            //         set.remove(it);
+            //     }
+            // }
+            
+            // System.out.println(set);
+            
+            
+            if(endWord.equals(stringData))
+            {
+                if(temp.size()<=min)
+                {
+                    ans.add(temp);
+                    min=temp.size();
                 }
             }
-
-            String word = vec.get(vec.size() - 1);
-
-            // store the answers if the end word matches with targetWord.
-            if (word.equals(targetWord)) {
-                // the first sequence where we reached the end.
-                if (ans.size() == 0) {
-                    ans.add(vec);
-                } else if (ans.get(0).size() == vec.size()) {
-                    ans.add(vec);
-                }
-            }
-            for (int i = 0; i < word.length(); i++) {
+            
+            // not so special code snippet
+            
+            // if(stringData.equals(targetWord)) {
+            //     // the first sequence where we reached the end.
+            //     if (ans.size() == 0) {
+            //         ans.add(temp);
+            //     } else if (ans.get(0).size() == temp.size()) {
+            //         ans.add(temp);
+            //     }
+            // }
+            
+            // for(int i=0;i<stringData.length();i++)
+            // {
+            //     for(char j='a';j<='z';j++)
+            //     {
+            //         String str=stringData.substring(0,i)+j+stringData.substring(i+1);
+            //         if(set.contains(str))
+            //         {
+            //             delete.add(str);
+            //             newList=new ArrayList<>(temp);
+            //             newList.add(str);
+            //             q.add(newList);
+            //         }
+            //     }
+            // }
+            
+            for (int i = 0; i < stringData.length(); i++) {
 
                 // Now, replace each character of ‘word’ with char
                 // from a-z then check if ‘word’ exists in wordList.
-                for (char c = 'a'; c <= 'z'; c++) {
-                    char replacedCharArray[] = word.toCharArray();
-                    replacedCharArray[i] = c;
+                for (char j = 'a'; j <= 'z'; j++) {
+                    char replacedCharArray[] = stringData.toCharArray();
+                    replacedCharArray[i] = j;
                     String replacedWord = new String(replacedCharArray);
-                    if (st.contains(replacedWord) == true) {
-                        vec.add(replacedWord);
+                    if (set.contains(replacedWord) == true) {
+                        temp.add(replacedWord);
                         // Java works by reference, so enter the copy of vec
                         // otherwise if you remove word from vec in next lines, it will 
                         // remove from everywhere 
-                        ArrayList < String > temp = new ArrayList < > (vec);
-                        q.add(temp);
+                        newList = new ArrayList < > (temp);
+                        q.add(newList);
                         // mark as visited on the level 
-                        usedOnLevel.add(replacedWord);
-                        vec.remove(vec.size() - 1);
+                        delete.add(replacedWord);
+                        temp.remove(temp.size() - 1);
                     }
                 }
 
