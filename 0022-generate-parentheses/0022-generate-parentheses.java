b9288b4d89fca34141ac1fe3d311d59ae7c6a0b3
Ujval Gupta <134779004+ug-is-og@users.noreply.github.com>
2023-11-04 16:36:24 +0530

    Time: 1 ms (89.25%), Space: 43.6 MB (23.43%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -1,40 +1,72 @@
+// Brute force appraoch, generating all possible paranthesis and eventually validating them
+
+// class Solution {
+//     public List<String> generateParenthesis(int n) {
+//         List<String> finalList=new ArrayList<>();
+//         helper(n*2,"",finalList);
+//         return finalList;
+//     }
+//     public void helper(int symbols,String temp,List<String> finalList)
+//     {
+//         if(symbols==0)
+//         {
+//             if(validate(temp))
+//             finalList.add(temp);
+//             return;
+//         }
+//         // add opening symbol
+//         helper(symbols-1,temp+"(",finalList);
+//         // add closing bracket
+//         helper(symbols-1,temp+")",finalList);
+//     }
+//     public boolean validate(String temp)
+//     {
+//         Stack<Character> st=new Stack<>();
+//         for(int i=0;i<temp.length();i++)
+//         {
+//             char c=temp.charAt(i);
+//             if(c=='(')
+//             st.add('(');
+//             else
+//             {
+//                 if(st.isEmpty()||st.peek()!='(')
+//                 return false;
+//                 else
+//                 st.pop();
+//             }
+//         }
+//         if(st.isEmpty())
+//         return true;
+//         return false;
+//     }
+// }
+
+// generating the valid paranthesis in the first place
+// for open paranthesis , open>0
+// for close paranthesis, close>0 and open<close
+
 class Solution {
-    public List<String> generateParenthesis(int n) {
-        List<String> finalList=new ArrayList<>();
-        helper(n*2,"",finalList);
-        return finalList;
-    }
-    public void helper(int symbols,String temp,List<String> finalList)
+    
+    public List<String> generateParenthesis(int n) 
     {
-        if(symbols==0)
+        List<String> list=new ArrayList<>();
+        helper(n,n,list,"");
+        return list;
+    }
+    public void helper(int open,int close,List<String> list,String s)
+    {
+        if(open==0&&close==0)
         {
-            if(validate(temp))
-            finalList.add(temp);
+            list.add(s);
             return;
         }
-        // add opening symbol
-        helper(symbols-1,temp+"(",finalList);
-        // add closing bracket
-        helper(symbols-1,temp+")",finalList);
-    }
-    public boolean validate(String temp)
-    {
-        Stack<Character> st=new Stack<>();
-        for(int i=0;i<temp.length();i++)
+        if(open>0)
         {
-            char c=temp.charAt(i);
-            if(c=='(')
-            st.add('(');
-            else
-            {
-                if(st.isEmpty()||st.peek()!='(')
-                return false;
-                else
-                st.pop();
-            }
+            helper(open-1,close,list,s+"(");
         }
-        if(st.isEmpty())
-        return true;
-        return false;
+        if(close>0&&open<close)
+        {
+            helper(open,close-1,list,s+")");
+        }
     }
 }
