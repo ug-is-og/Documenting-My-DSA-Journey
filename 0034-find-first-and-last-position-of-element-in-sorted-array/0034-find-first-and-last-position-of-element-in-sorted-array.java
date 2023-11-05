99ef5d737a261aabfdbbb3e48f458dcf7522e24f
Ujval Gupta <134779004+ug-is-og@users.noreply.github.com>
2023-11-06 00:26:04 +0530

    Time: 0 ms (100.00%), Space: 44.1 MB (88.21%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,32 +1,37 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int occurence[]=new int[2];
-        int start=0,end=nums.length-1;
-        boolean isFirst=true;
-        occurence[0]=targetOccurence(nums,start,end,target,true);
-        occurence[1]=targetOccurence(nums,start,end,target,false);
-        return occurence;
+        int ans[]=new int[2];
+        ans[0]=binarySearch(nums,target,true); // startIndex represented by set bit
+        ans[1]=binarySearch(nums,target,false);
+        return ans;
     }
-    public int targetOccurence(int nums[],int start,int end,int target,boolean isFirst)
+    public int binarySearch(int nums[],int target,boolean start)
     {
-       int mid;
-        int occurence=-1;
-        while(start<=end)
+        int index=-1,lo=0,hi=nums.length-1;
+        while(lo<=hi)
         {
-            mid=(start+end)/2;
+            int mid=(lo+hi)/2;
             if(nums[mid]==target)
             {
-                occurence=mid;
-                if(isFirst)
-                end=mid-1;
+                index=mid;
+                if(start)
+                {
+                    hi=mid-1;
+                }
                 else
-                start=mid+1;
+                {
+                    lo=mid+1;
+                }
             }
-            else if(nums[mid]<target)
-            start=mid+1;
+            else if(nums[mid]>target)
+            {
+                hi=mid-1;
+            }
             else
-            end=mid-1;
+            {
+                lo=mid+1;
+            }
         }
-        return occurence;
+        return index;
     }
 }
