a88aae69d8e33eddebff42d96e0f306b08755a47
Ujval Gupta <134779004+ug-is-og@users.noreply.github.com>
2023-11-11 23:50:16 +0530

    Time: 1 ms (99.51%), Space: 43.6 MB (82.82%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -47,21 +47,68 @@
 // let's memoize this approach
 // we'll optimize this solution later, as a follow up is given to do in O(n) space
 
+// class Solution {
+//     public int minimumTotal(List<List<Integer>> triangle) {
+//         int dp[][]=new int[triangle.size()][triangle.get(triangle.size()-1).size()];
+//         for(int temp[]:dp)
+//         Arrays.fill(temp,-1);
+//         return helper(0,0,triangle,dp);
+//     }
+//     public int helper(int rowIndex,int colIndex,List<List<Integer>> triangle,int dp[][])
+//     {
+//         if(rowIndex==triangle.size())
+//         return 0;
+//         if(dp[rowIndex][colIndex]!=-1)
+//         return dp[rowIndex][colIndex];
+//         int way1=helper(rowIndex+1,colIndex,triangle,dp);
+//         int way2=helper(rowIndex+1,colIndex+1,triangle,dp);
+//         return dp[rowIndex][colIndex]=Math.min(way1,way2)+triangle.get(rowIndex).get(colIndex);
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
-        int dp[][]=new int[triangle.size()][triangle.get(triangle.size()-1).size()];
-        for(int temp[]:dp)
-        Arrays.fill(temp,-1);
-        return helper(0,0,triangle,dp);
+       int dp[][]=new int[triangle.size()][triangle.get(triangle.size()-1).size()];
+       for(int temp[]:dp)
+       {
+           Arrays.fill(temp,-1);
+       }
+       return helper(triangle,0,0,dp);
     }
-    public int helper(int rowIndex,int colIndex,List<List<Integer>> triangle,int dp[][])
+    public int helper(List<List<Integer>> triangle,int i,int j,int dp[][])
     {
-        if(rowIndex==triangle.size())
-        return 0;
-        if(dp[rowIndex][colIndex]!=-1)
-        return dp[rowIndex][colIndex];
-        int way1=helper(rowIndex+1,colIndex,triangle,dp);
-        int way2=helper(rowIndex+1,colIndex+1,triangle,dp);
-        return dp[rowIndex][colIndex]=Math.min(way1,way2)+triangle.get(rowIndex).get(colIndex);
+        if(i==triangle.size())
+        {
+            return 0;
+        }
+        if(dp[i][j]!=-1)
+        {
+            return dp[i][j];
+        }
+        return dp[i][j]=triangle.get(i).get(j)+Math.min(helper(triangle,i+1,j,dp),helper(triangle,i+1,j+1,dp));
     }
 }
