55ae2e647b045af13e0d8caadf15689c382d70ef
Ujval Gupta <134779004+ug-is-og@users.noreply.github.com>
2023-11-16 02:55:21 +0530

    Time: 0 ms (100.00%), Space: 41 MB (21.89%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.java
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.java
@@ -1,25 +1,70 @@
+// class Solution {
+//     public int findMin(int[] arr) {
+//         int start=0,end=arr.length-1;
+//         int mid=0;
+//         int min=Integer.MAX_VALUE;
+//         while(start<=end)
+//         {
+//             if(arr[start]<=arr[end]) // optimization for a better time complexity, matlab zabardasti binary search nahi karna padega
+//             {
+//                 min=Math.min(arr[start],min);
+//                 return min;
+//             }
+//             mid=(start+end)/2;
+//             if(arr[start]<=arr[mid])
+//             {
+//                 min=Math.min(min,arr[start]); // left sorted half ke min value (arr[low]) se compare karke right chale ja re
+//                 start=mid+1;
+//             }
+//             else
+//             {
+//                 min=Math.min(min,arr[mid]); // right sorted half ke min value (arr[mid]) se compare karke left mein chale ja re
+//                 end=mid-1;
+//             }
+//         }
+//         return min;
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 class Solution {
     public int findMin(int[] arr) {
-        int start=0,end=arr.length-1;
-        int mid=0;
-        int min=Integer.MAX_VALUE;
-        while(start<=end)
+        int lo=0,hi=arr.length-1,min=Integer.MAX_VALUE;
+        int mid=(lo+hi)/2;
+        if(arr[mid]>=arr[lo]&&arr[mid]<=arr[hi])
+        return arr[0];
+        while(lo<=hi)
         {
-            if(arr[start]<=arr[end]) // optimization for a better time complexity, matlab zabardasti binary search nahi karna padega
+            mid=(lo+hi)/2;
+            if(arr[mid]>=arr[lo])
             {
-                min=Math.min(arr[start],min);
-                return min;
+                min=Math.min(min,arr[lo]);
+                lo=mid+1;
             }
-            mid=(start+end)/2;
-            if(arr[start]<=arr[mid])
+            else if(arr[mid]<=arr[hi])
             {
-                min=Math.min(min,arr[start]); // left sorted half ke min value (arr[low]) se compare karke right chale ja re
-                start=mid+1;
-            }
-            else
-            {
-                min=Math.min(min,arr[mid]); // right sorted half ke min value (arr[mid]) se compare karke left mein chale ja re
-                end=mid-1;
+                min=Math.min(min,arr[mid]);
+                hi=mid-1;
             }
         }
         return min;
