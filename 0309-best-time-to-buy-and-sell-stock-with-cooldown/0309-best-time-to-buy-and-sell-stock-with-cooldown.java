41903372a340dea81b462212aa9e1cd4d6587ac1
Ujval Gupta <134779004+ug-is-og@users.noreply.github.com>
2023-11-17 01:13:15 +0530

    Time: 1 ms (86.86%), Space: 40.8 MB (27.45%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.java
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -1,3 +1,55 @@
+// class Solution {
+//     public int maxProfit(int[] prices) {
+//         int dp[][]=new int[prices.length][2];
+//         for(int temp[]:dp)
+//         {
+//             Arrays.fill(temp,-1);
+//         }
+//         return helper(prices,0,1,dp); // 1 means buy and 0 means sell
+//     }
+//     public int helper(int prices[],int index,int status,int dp[][])
+//     {
+//         if(index>=prices.length)
+//         {
+//             return 0;
+//         }
+//         if(dp[index][status]!=-1)
+//         {
+//             return dp[index][status];
+//         }
+//         int ans=0;
+//         if(status==1)
+//         {
+//             ans=Math.max(-prices[index]+helper(prices,index+1,0,dp),helper(prices,index+1,1,dp));
+//         }
+//         else
+//         {
+//             ans=Math.max(prices[index]+helper(prices,index+2,1,dp),helper(prices,index+1,0,dp));
+//         }
+//         return dp[index][status]=ans;
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 class Solution {
     public int maxProfit(int[] prices) {
         int dp[][]=new int[prices.length][2];
@@ -5,27 +57,27 @@ class Solution {
         {
             Arrays.fill(temp,-1);
         }
-        return helper(prices,0,1,dp); // 1 means buy and 0 means sell
+       return helper(prices,0,1,dp); // 1 means I have to buy
     }
-    public int helper(int prices[],int index,int status,int dp[][])
+    public int helper(int prices[],int index,int buyStatus,int dp[][])
     {
         if(index>=prices.length)
         {
             return 0;
         }
-        if(dp[index][status]!=-1)
+        if(dp[index][buyStatus]!=-1)
         {
-            return dp[index][status];
+            return dp[index][buyStatus];
         }
-        int ans=0;
-        if(status==1)
+        int profit=0;
+        if(buyStatus==1)
         {
-            ans=Math.max(-prices[index]+helper(prices,index+1,0,dp),helper(prices,index+1,1,dp));
+            profit=Math.max(-prices[index]+helper(prices,index+1,0,dp),helper(prices,index+1,1,dp));
         }
         else
         {
-            ans=Math.max(prices[index]+helper(prices,index+2,1,dp),helper(prices,index+1,0,dp));
+            profit=Math.max(prices[index]+helper(prices,index+2,1,dp),helper(prices,index+1,0,dp));
         }
-        return dp[index][status]=ans;
+        return dp[index][buyStatus]=profit;
     }
 }
