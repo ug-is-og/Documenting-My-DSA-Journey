41338d31e4b294b7b5366793f1c52cf17f36fcef
Ujval Gupta <134779004+ug-is-og@users.noreply.github.com>
2023-11-24 23:33:59 +0530

    Time: 4 ms (65.75%), Space: 43.7 MB (5.51%) - LeetHub

--- 2829-determine-the-minimum-sum-of-a-k-avoiding-array/2829-determine-the-minimum-sum-of-a-k-avoiding-array.java
+++ 2829-determine-the-minimum-sum-of-a-k-avoiding-array/2829-determine-the-minimum-sum-of-a-k-avoiding-array.java
@@ -1,19 +1,58 @@
+// class Solution {
+//     public int minimumSum(int n, int k) {
+//         HashMap<Integer,Integer> map=new HashMap<>();
+//         int tempCount=2;
+//         map.put(k-1,1);
+//         int sum=1;
+//         while(map.size()<n)
+//         {
+//             if(!map.containsKey(tempCount))
+//             {
+//                 map.put(k-tempCount,tempCount);
+//                 sum=sum+tempCount;
+//             }
+//             tempCount++;
+            
+//         }
+//         return sum;
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 class Solution {
     public int minimumSum(int n, int k) {
-        HashMap<Integer,Integer> map=new HashMap<>();
-        int tempCount=2;
-        map.put(k-1,1);
-        int sum=1;
-        while(map.size()<n)
-        {
-            if(!map.containsKey(tempCount))
-            {
-                map.put(k-tempCount,tempCount);
-                sum=sum+tempCount;
-            }
-            tempCount++;
-            
-        }
-        return sum;
+       HashMap<Integer,Integer> map=new HashMap<>();
+       int count=0,i=1,sum=0;
+       while(count!=n)
+       {
+           if(!map.containsKey(k-i))
+           {
+               map.put(i,1);
+               count++;
+               sum=sum+i;
+           }
+           i++;
+       }
+       return sum;
     }
 }
