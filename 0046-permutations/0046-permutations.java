30e23a85833f941a3a7aff935651df266c3a21a1
Ujval Gupta <134779004+ug-is-og@users.noreply.github.com>
2023-11-26 12:25:58 +0530

    Time: 1 ms (94.43%), Space: 44.5 MB (7.51%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -33,33 +33,80 @@
 
 // Method 2 (Swapping)
 
+// class Solution {
+//     public List<List<Integer>> permute(int[] nums) {
+//         List<List<Integer>> list=new ArrayList<>();
+//         helper(0,nums,list);
+//         return list;
+//     }
+//     public void helper(int index,int nums[],List<List<Integer>> list)
+//     {
+//         if(index==nums.length)
+//         {
+//             List<Integer> tempList=new ArrayList<>();
+//             for(int i=0;i<nums.length;i++)
+//             tempList.add(nums[i]);
+//             list.add(tempList);
+//             return;
+//         }
+//         for(int i=index;i<nums.length;i++)
+//         {
+//             // swap index element with ith index
+//             int data=nums[index];
+//             nums[index]=nums[i];
+//             nums[i]=data;
+//             helper(index+1,nums,list);
+//             // swap back
+//             data=nums[i];
+//             nums[i]=nums[index];
+//             nums[index]=data;
+//         }
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// swapping method
+
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
-        List<List<Integer>> list=new ArrayList<>();
-        helper(0,nums,list);
-        return list;
+        List<List<Integer>> ans=new ArrayList<>();
+        backTrack(ans,0,nums);
+        return ans;
     }
-    public void helper(int index,int nums[],List<List<Integer>> list)
+    public void backTrack(List<List<Integer>> ans,int index,int nums[])
     {
         if(index==nums.length)
         {
-            List<Integer> tempList=new ArrayList<>();
-            for(int i=0;i<nums.length;i++)
-            tempList.add(nums[i]);
-            list.add(tempList);
-            return;
+            List<Integer> temp=new ArrayList<>();
+            for(int a:nums)
+            temp.add(a);
+            ans.add(temp);
         }
         for(int i=index;i<nums.length;i++)
         {
-            // swap index element with ith index
-            int data=nums[index];
-            nums[index]=nums[i];
-            nums[i]=data;
-            helper(index+1,nums,list);
-            // swap back
-            data=nums[i];
+            // swap elements;
+            int temp=nums[i];
             nums[i]=nums[index];
-            nums[index]=data;
+            nums[index]=temp;
+            backTrack(ans,index+1,nums);
+            //swap back
+            temp=nums[i];
+            nums[i]=nums[index];
+            nums[index]=temp;
         }
     }
 }
