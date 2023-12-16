26a260bbd5a01cef1e12a7118e4288d5c11f7b26
Ujval Gupta <134779004+ug-is-og@users.noreply.github.com>
2023-12-16 10:49:17 +0530

    Time: 4 ms (73.04%), Space: 41.2 MB (99.88%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -52,38 +52,66 @@
 
 
 
+// O(n) TC and O(n) space (using map)
+
+// class Solution {
+//     public boolean isAnagram(String s, String t) {
+//        HashMap<Character,Integer> map=new HashMap<>();
+//        for(int i=0;i<s.length();i++)
+//        {
+//            char c=s.charAt(i);
+//            map.put(c,map.getOrDefault(c,0)+1);
+//        }
+//        for(int i=0;i<t.length();i++)
+//        {
+//            char c=t.charAt(i);
+//            if(map.containsKey(c))
+//            {
+//                if(map.get(c)==1)
+//                {
+//                    map.remove(c);
+//                }
+//                else
+//                {
+//                    map.put(c,map.get(c)-1);
+//                }
+//            }
+//            else
+//            {
+//                return false;
+//            }
+//        }
+//        if(map.isEmpty())
+//        {
+//            return true;
+//        }
+//        return false;
+//     }
+// }
+
+
+// using array
 
 class Solution {
     public boolean isAnagram(String s, String t) {
-       HashMap<Character,Integer> map=new HashMap<>();
+       int freq[]=new int[26];
        for(int i=0;i<s.length();i++)
        {
-           char c=s.charAt(i);
-           map.put(c,map.getOrDefault(c,0)+1);
+           int index=((int)s.charAt(i))-97;
+           freq[index]++;
        }
        for(int i=0;i<t.length();i++)
        {
-           char c=t.charAt(i);
-           if(map.containsKey(c))
-           {
-               if(map.get(c)==1)
-               {
-                   map.remove(c);
-               }
-               else
-               {
-                   map.put(c,map.get(c)-1);
-               }
-           }
-           else
-           {
-               return false;
-           }
+           int index=((int)t.charAt(i))-97;
+           if(freq[index]==0)
+           return false;
+           freq[index]--;
        }
-       if(map.isEmpty())
+       for(int i=0;i<freq.length;i++)
        {
-           return true;
+           if(freq[i]!=0)
+           return false;
        }
-       return false;
+       return true;
     }
 }
