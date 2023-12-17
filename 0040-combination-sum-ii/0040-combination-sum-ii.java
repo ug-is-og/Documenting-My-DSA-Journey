887cb4aa777f2739c7bb71363cdc9ba615041820
Ujval Gupta <134779004+ug-is-og@users.noreply.github.com>
2023-12-18 02:57:12 +0530

    Time: 3 ms (77.29%), Space: 42.1 MB (82.54%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.java
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -41,33 +41,99 @@
 // insight for optimization, hame bol rakha hai ki ek combination mein ek array element ko ek hi baar le sakte hai
 //https://youtu.be/G1fRTGRxXU8 (Striver's approach)
 
+// class Solution {
+//     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+//          List<List<Integer>> finalAns=new ArrayList<>();
+//         List<Integer> temp=new ArrayList<>();
+//         Arrays.sort(candidates); 
+//         helper(0,candidates,finalAns,target,temp);
+//         return finalAns;
+//     }
+//     public void helper(int index,int candidates[],List<List<Integer>> finalAns,int target,List<Integer> temp)
+//     {
+//         if(target==0)
+//         {
+//             finalAns.add(new ArrayList<>(temp));
+//             return ;
+//         }
+//         if(target<0)
+//         return;
+//         for(int i=index;i<candidates.length;i++)
+//         {
+//             // iss condition se agar [1,1,1,2,2] hai and target=4 toh mein duplicate calls lagane se bach ja raha hun, matlab 1 liya aur ab seedha 2 pe chale gaye...kyun ...kyunki baaki ke 2 1's recursive calls mein cover ho jayenge and agar recursive calls mein cover ho chuke hai toh loop mein wapas kyun consider karoge..kyunki agar loop mein aake wapas add karoge toh duplicate calls ho jayengi..toh tum unnecesaarilty kyun maaroge duplicate call...upar wale code mein unnecessarily bhot saari calls lag ri...isme kewal useful calls mein ho ja ra karyakram, matlab upar wale code mein tumne take no take ka rona kar diya hai, yahan woh nahi karna padega
+//             if(i>index&&candidates[i]==candidates[i-1])
+//             continue;
+//             if(candidates[i]>target)
+//             break;
+//             temp.add(candidates[i]);
+//             helper(i+1,candidates,finalAns,target-candidates[i],temp);
+//             temp.remove(temp.size()-1);
+//         }
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// meri approach bhi striver se milti jhulti hai par khud se likhi so great job
+
 class Solution {
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
-         List<List<Integer>> finalAns=new ArrayList<>();
-        List<Integer> temp=new ArrayList<>();
-        Arrays.sort(candidates); 
-        helper(0,candidates,finalAns,target,temp);
-        return finalAns;
+        List<List<Integer>> ans=new ArrayList<>();
+        Arrays.sort(candidates);
+        helper(ans,new ArrayList<>(),0,candidates,target);
+        return ans;
     }
-    public void helper(int index,int candidates[],List<List<Integer>> finalAns,int target,List<Integer> temp)
-    {
-        if(target==0)
+    public void helper(List<List<Integer>> ans,List<Integer> temp,int index,int candidates[],int remSum){
+        if(remSum==0)
         {
-            finalAns.add(new ArrayList<>(temp));
-            return ;
+            ans.add(new ArrayList<>(temp));
+            return;
         }
-        if(target<0)
-        return;
-        for(int i=index;i<candidates.length;i++)
+        if(index==candidates.length||remSum<candidates[index]||remSum<0)
         {
-            // iss condition se agar [1,1,1,2,2] hai and target=4 toh mein duplicate calls lagane se bach ja raha hun, matlab 1 liya aur ab seedha 2 pe chale gaye...kyun ...kyunki baaki ke 2 1's recursive calls mein cover ho jayenge and agar recursive calls mein cover ho chuke hai toh loop mein wapas kyun consider karoge..kyunki agar loop mein aake wapas add karoge toh duplicate calls ho jayengi..toh tum unnecesaarilty kyun maaroge duplicate call...upar wale code mein unnecessarily bhot saari calls lag ri...isme kewal useful calls mein ho ja ra karyakram, matlab upar wale code mein tumne take no take ka rona kar diya hai, yahan woh nahi karna padega
-            if(i>index&&candidates[i]==candidates[i-1])
-            continue;
-            if(candidates[i]>target)
-            break;
-            temp.add(candidates[i]);
-            helper(i+1,candidates,finalAns,target-candidates[i],temp);
+            return;
+        }
+        int i=0;
+        for(i=index;i<candidates.length;i++)
+        {
+            if(candidates[i]==candidates[index])
+            {
+                continue;
+            }
+            else
+            {
+                break;
+            }
+        }
+        helper(ans,temp,i,candidates,remSum);
+        for(int k=index;k<i;k++)
+        {
+            temp.add(candidates[k]);
+            helper(ans,temp,i,candidates,remSum-(k-index+1)*candidates[k]);
+        }
+        for(int k=index;k<i;k++)
+        {
             temp.remove(temp.size()-1);
         }
     }
 }
+
