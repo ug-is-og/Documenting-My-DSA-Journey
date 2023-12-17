c976e0901818e48c5ca9784f61d537f4bbaf588d
Ujval Gupta <134779004+ug-is-og@users.noreply.github.com>
2023-12-18 01:29:48 +0530

    Time: 2 ms (75.67%), Space: 44.1 MB (27.62%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,30 +1,72 @@
+// class Solution {
+//     public List<List<Integer>> combinationSum(int[] candidates, int target) {
+//         List<Integer> temp=new ArrayList<>();
+//         List<List<Integer>> finalAns=new ArrayList<>();
+//         helper(0,candidates,target,temp,finalAns);
+//         return finalAns;
+//     }
+//     public void helper(int index,int candidates[],int target,List<Integer> temp,List<List<Integer>> finalAns)
+//     {
+//         if(target==0)
+//         {
+//             List<Integer> newTemp=new ArrayList<>(temp);
+//             finalAns.add(newTemp);
+//             return;
+//         }
+//         if(index==candidates.length)
+//         return;
+//         if(target<candidates[index])
+//         {
+//             helper(index+1,candidates,target,temp,finalAns);
+//         }
+//         else
+//         {
+//             helper(index+1,candidates,target,temp,finalAns);
+//             temp.add(candidates[index]);
+//             helper(index,candidates,target-candidates[index],temp,finalAns);
+//             temp.remove(temp.size()-1);
+//         }
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// upar wali approacha jaise hi kiya hai bs upar ham pehle hi check kar le re ki remSum, current element se bada hai ya chhota and accordingly calls laga re
+
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<Integer> temp=new ArrayList<>();
-        List<List<Integer>> finalAns=new ArrayList<>();
-        helper(0,candidates,target,temp,finalAns);
-        return finalAns;
+        List<List<Integer>> ans=new ArrayList<>();
+        helper(0,ans,new ArrayList<>(),candidates,target);
+        return ans;
     }
-    public void helper(int index,int candidates[],int target,List<Integer> temp,List<List<Integer>> finalAns)
+    public void helper(int index,List<List<Integer>> ans,List<Integer> temp,int candidates[],int remSum)
     {
-        if(target==0)
+        if(remSum==0)
         {
-            List<Integer> newTemp=new ArrayList<>(temp);
-            finalAns.add(newTemp);
+            ans.add(new ArrayList<>(temp));
             return;
         }
-        if(index==candidates.length)
-        return;
-        if(target<candidates[index])
+        if(index==candidates.length||remSum<0)
         {
-            helper(index+1,candidates,target,temp,finalAns);
+            return;
         }
-        else
-        {
-            helper(index+1,candidates,target,temp,finalAns);
-            temp.add(candidates[index]);
-            helper(index,candidates,target-candidates[index],temp,finalAns);
-            temp.remove(temp.size()-1);
-        }
+        temp.add(candidates[index]);
+        helper(index,ans,temp,candidates,remSum-candidates[index]);
+        temp.remove(temp.size()-1);
+        helper(index+1,ans,temp,candidates,remSum);
     }
+
 }
