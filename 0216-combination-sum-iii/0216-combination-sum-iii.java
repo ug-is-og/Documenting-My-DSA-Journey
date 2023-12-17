7db5d02f1a1bbd92027f6cc2eb8b9c0fd735804e
Ujval Gupta <134779004+ug-is-og@users.noreply.github.com>
2023-12-18 03:15:56 +0530

    Time: 0 ms (100.00%), Space: 40.6 MB (15.83%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.java
+++ 0216-combination-sum-iii/0216-combination-sum-iii.java
@@ -76,26 +76,67 @@
 // for optimal approach, ab jo bhi optimizations honi hai woh above code mein hi hongi
 // this is all
 
+// class Solution {
+//     public List<List<Integer>> combinationSum3(int k, int n) {
+//         List<List<Integer>> ans=new ArrayList<>();
+//         List<Integer> temp=new ArrayList<>();
+//         helper(1,k,n,temp,ans);
+//         return ans;
+//     }
+//     public void helper(int index,int k,int remSum,List<Integer> temp,List<List<Integer>> ans)
+//     {
+//         if(index>9)
+//         {
+//           if(k==0&&remSum==0)
+//           ans.add(new ArrayList<>(temp));
+//           return;
+//         }
+//         // no take
+//         helper(index+1,k,remSum,temp,ans);
+//         // take
+//         temp.add(index);
+//         helper(index+1,k-1,remSum-index,temp,ans);
+//         temp.remove(temp.size()-1);
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+// most optimal code, beats 100%
+
 class Solution {
     public List<List<Integer>> combinationSum3(int k, int n) {
+        int arr[]={1,2,3,4,5,6,7,8,9};
         List<List<Integer>> ans=new ArrayList<>();
-        List<Integer> temp=new ArrayList<>();
-        helper(1,k,n,temp,ans);
+        helper(ans,new ArrayList<>(),arr,k,n,0);
         return ans;
     }
-    public void helper(int index,int k,int remSum,List<Integer> temp,List<List<Integer>> ans)
+    public void helper(List<List<Integer>> ans,List<Integer> temp,int arr[],int k,int n,int index)
     {
-        if(index>9)
+        if(n==0)
         {
-          if(k==0&&remSum==0)
-          ans.add(new ArrayList<>(temp));
-          return;
+            if(temp.size()==k)
+            ans.add(new ArrayList<>(temp));
+            return;
         }
-        // no take
-        helper(index+1,k,remSum,temp,ans);
-        // take
-        temp.add(index);
-        helper(index+1,k-1,remSum-index,temp,ans);
-        temp.remove(temp.size()-1);
+        if(index==arr.length||n<arr[index])
+        {
+            return;
+        }
+        helper(ans,temp,arr,k,n,index+1);
+        if(temp.size()<k)
+        {
+            temp.add(arr[index]);
+            helper(ans,temp,arr,k,n-arr[index],index+1);
+            temp.remove(temp.size()-1);
+        }
     }
 }
