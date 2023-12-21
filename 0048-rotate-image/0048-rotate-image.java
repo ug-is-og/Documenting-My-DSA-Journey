12fee519522937b786a043952fb74234313b6396
Ujval Gupta <134779004+ug-is-og@users.noreply.github.com>
2023-12-22 00:22:40 +0530

    Time: 0 ms (100.00%), Space: 42.3 MB (5.09%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -45,30 +45,81 @@
 
 
 
+// class Solution {
+//     public void rotate(int[][] matrix) {
+//         int n=matrix.length;
+//         // rotate horizonatally
+//         for(int i=0;i<n/2;i++)
+//         {
+//             for(int j=0;j<n;j++)
+//             {
+//                 int temp=matrix[i][j];
+//                 matrix[i][j]=matrix[n-1-i][j];
+//                 matrix[n-1-i][j]=temp;
+//             }
+//         }
+//         //find transpose
+//         for(int i=0;i<n;i++)
+//         {
+//             for(int j=0;j<n;j++)
+//             {
+//                 if(j>i) 
+//                 {
+//                     int temp=matrix[i][j];
+//                     matrix[i][j]=matrix[j][i];
+//                     matrix[j][i]=temp;
+//                 }
+//             }
+//         }
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 class Solution {
     public void rotate(int[][] matrix) {
-        int n=matrix.length;
-        // rotate horizonatally
-        for(int i=0;i<n/2;i++)
+        // transpose of the matrix from (i,j) to (j,i)
+        for(int i=0;i<matrix.length;i++)
         {
-            for(int j=0;j<n;j++)
+            for(int j=0;j<matrix.length;j++)
             {
-                int temp=matrix[i][j];
-                matrix[i][j]=matrix[n-1-i][j];
-                matrix[n-1-i][j]=temp;
-            }
-        }
-        //find transpose
-        for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<n;j++)
-            {
-                if(j>i) 
+                if(i<j)
                 {
                     int temp=matrix[i][j];
                     matrix[i][j]=matrix[j][i];
                     matrix[j][i]=temp;
                 }
+            }
+        }
+        // swap columns
+        for(int i=0;i<matrix.length;i++)
+        {
+            for(int j=0;j<matrix[0].length/2;j++)
+            {
+                int temp=matrix[i][j];
+                matrix[i][j]=matrix[i][matrix[0].length-1-j];
+                matrix[i][matrix[0].length-1-j]=temp;
             }
         }
     }
