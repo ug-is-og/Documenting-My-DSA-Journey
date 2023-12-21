9b39eba4374ed5ffbef393eae21fe1eb680fa2ae
Ujval Gupta <134779004+ug-is-og@users.noreply.github.com>
2023-12-22 02:40:53 +0530

    Time: 27 ms (29.94%), Space: 47.1 MB (15.22%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -24,27 +24,68 @@
 
 // optimized solution
 
+// class Solution {
+//     public int subarraySum(int[] nums, int k) {
+//         int count=0;
+//         int prefix[]=new int[nums.length];
+//         HashMap<Integer,Integer> map=new HashMap<>();
+//         map.put(0,1);
+//         for(int i=0;i<nums.length;i++)
+//         {
+//             if(i==0)
+//             {
+//                 prefix[0]=nums[0];
+//             }
+//             else
+//             {
+//                 prefix[i]=prefix[i-1]+nums[i];
+//             }
+//             if(map.containsKey(prefix[i]-k))
+//             count=count+map.get(prefix[i]-k);
+//             map.put(prefix[i],map.getOrDefault(prefix[i],0)+1);
+//             // if(map.containsKey(prefix[i]-k)) // khaas dhyan rakhna iss cheez ka considering TC : [1], 0 
+//             // count++;
+//         }
+//         return count;
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 class Solution {
     public int subarraySum(int[] nums, int k) {
         int count=0;
-        int prefix[]=new int[nums.length];
+        int prefixSum[]=new int[nums.length];
         HashMap<Integer,Integer> map=new HashMap<>();
-        map.put(0,1);
         for(int i=0;i<nums.length;i++)
         {
             if(i==0)
             {
-                prefix[0]=nums[0];
+                prefixSum[i]=nums[i];
             }
             else
             {
-                prefix[i]=prefix[i-1]+nums[i];
+                prefixSum[i]=prefixSum[i-1]+nums[i];
             }
-            if(map.containsKey(prefix[i]-k))
-            count=count+map.get(prefix[i]-k);
-            map.put(prefix[i],map.getOrDefault(prefix[i],0)+1);
-            // if(map.containsKey(prefix[i]-k)) // khaas dhyan rakhna iss cheez ka considering TC : [1], 0 
-            // count++;
+            if(prefixSum[i]==k) // very important
+            count++;
+            if(map.containsKey(prefixSum[i]-k))
+            {
+                count=count+map.get(prefixSum[i]-k);
+            }
+            map.put(prefixSum[i],map.getOrDefault(prefixSum[i],0)+1);
         }
         return count;
     }
