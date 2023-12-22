941a428f19fd5af8c3abdc7b25e2109f852f3803
Ujval Gupta <134779004+ug-is-og@users.noreply.github.com>
2023-12-23 01:16:23 +0530

    Time: 0 ms (100.00%), Space: 41.6 MB (5.64%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.java
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.java
@@ -8,15 +8,42 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+// class Solution {
+//     public ListNode middleNode(ListNode head) {
+//         ListNode slow=head;
+//         ListNode fast=head;
+//         // yeh condition ratne ki koi zarurat nahi, dikakt ho jayegi , iss question mein hame bola tha ki hame even size ke cases mein second mid chahiye toh uss hisaab se kaam banaya
+//         // don't mug up
+//         while(fast!=null&&fast.next!=null)// yeh bhot dhyan se dekhna agar condition mein
+//         // ordering change kardi toh ans galat aa jayega, agar (fast.next!=null&&fast!=null)
+//         // kiya toh tum null ka next check karne lage for even sized linked list toh thoda sambhal ke
+//         {
+//             slow=slow.next;
+//             fast=fast.next.next;
+//         }
+//         return slow;
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 class Solution {
     public ListNode middleNode(ListNode head) {
-        ListNode slow=head;
-        ListNode fast=head;
-        // yeh condition ratne ki koi zarurat nahi, dikakt ho jayegi , iss question mein hame bola tha ki hame even size ke cases mein second mid chahiye toh uss hisaab se kaam banaya
-        // don't mug up
-        while(fast!=null&&fast.next!=null)// yeh bhot dhyan se dekhna agar condition mein
-        // ordering change kardi toh ans galat aa jayega, agar (fast.next!=null&&fast!=null)
-        // kiya toh tum null ka next check karne lage for even sized linked list toh thoda sambhal ke
+        ListNode slow=head,fast=head;
+        while(fast!=null&&fast.next!=null)
         {
             slow=slow.next;
             fast=fast.next.next;
