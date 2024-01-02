edd58d78f97ddc57b2822682bee28da7370944cf
Ujval Gupta <134779004+ug-is-og@users.noreply.github.com>
2024-01-02 12:35:39 +0530

    Added solution - LeetHub

--- Detect cycle in an undirected graph - GFG/detect-cycle-in-an-undirected-graph.java
+++ Detect cycle in an undirected graph - GFG/detect-cycle-in-an-undirected-graph.java
@@ -34,51 +34,195 @@ class GFG {
 // using BFS approach
 
 // you didn't keep in mind the connected components case
+// matlab yaar tumhe ek for loop lagake harr connected componene tmein jaba hoga, pehlke tum kewal 0 ko source maanke chala de re the
+// toh agar koi aur component connected hua ans usme cycle huii toh tumhe pata hi nahi chalega
 
-class data{
-    int child;
-    int parent;
-    public data(int child,int parent){
-        this.child=child;
-        this.parent=parent;
-    }
-}
+// class data{
+//     int child;
+//     int parent;
+//     public data(int child,int parent){
+//         this.child=child;
+//         this.parent=parent;
+//     }
+// }
+// class Solution {
+//     // Function to detect cycle in an undirected graph.
+//     public boolean isCycle(int V, ArrayList<ArrayList<Integer>> adj) {
+//       boolean visited[]=new boolean[V];
+//       boolean ans=false;
+//       for(int i=0;i<V;i++)
+//       {
+//           if(!visited[i])
+//           {
+//               visited[i]=true;
+//               ans=ans||helper(visited,adj,i);
+//           }
+//       }
+//       return ans;
+//     }
+//     public boolean helper(boolean visited[],ArrayList<ArrayList<Integer>> adj,int source)
+//     {
+//         Queue<data> q=new LinkedList<>();
+//         q.add(new data(source,-1));
+//         while(!q.isEmpty())
+//         {
+//             data temp=q.remove();
+//             for(int i=0;i<adj.get(temp.child).size();i++)
+//             {
+//                 if(!visited[adj.get(temp.child).get(i)])
+//                 {
+//                     visited[adj.get(temp.child).get(i)]=true;
+//                     q.add(new data(adj.get(temp.child).get(i),temp.child));
+//                 }
+//                 else
+//                 {
+//                     if(adj.get(temp.child).get(i)!=temp.parent)
+//                     {
+//                         return true;
+//                     }
+//                 }
+//             }
+//         }
+//         return false;
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // yahan pe firse connected components wala case bhul gaye the
+
+// class node{
+//     int child;
+//     int parent;
+//     public node(int child,int parent)
+//     {
+//         this.child=child;
+//         this.parent=parent;
+//     }
+// }
+
+// class Solution {
+//     // Function to detect cycle in an undirected graph.
+//     public boolean isCycle(int V, ArrayList<ArrayList<Integer>> adj) {
+//       Queue<node> q=new LinkedList<>();
+//       boolean visited[]=new boolean[V];
+//       for(int i=0;i<V;i++)
+//       {
+//           if(!visited[i])
+//           {
+//               visited[i]=true;
+//               if(helper(q,visited,i,adj))
+//               return true;
+//           }
+//       }
+//       return false;
+//     }
+//     public boolean helper(Queue<node> q,boolean visited[],int src,ArrayList<ArrayList<Integer>> adj)
+//     {
+//       q.add(new node(src,-1));
+//       while(!q.isEmpty())
+//       {
+//           node temp=q.remove();
+//           for(int i=0;i<adj.get(temp.child).size();i++)
+//           {
+//               if(adj.get(temp.child).get(i)!=temp.parent&&visited[adj.get(temp.child).get(i)])
+//               {
+//                   return true;
+//               }
+//               if(!visited[adj.get(temp.child).get(i)])
+//               {
+//                   q.add(new node(adj.get(temp.child).get(i),temp.child));
+//                   visited[adj.get(temp.child).get(i)]=true;
+//               }
+//           }
+//       }
+//       return false;
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// lets go with DFS approach first
+
 class Solution {
     // Function to detect cycle in an undirected graph.
     public boolean isCycle(int V, ArrayList<ArrayList<Integer>> adj) {
-       boolean visited[]=new boolean[V];
-       boolean ans=false;
-       for(int i=0;i<V;i++)
-       {
-           if(!visited[i])
-           {
-               visited[i]=true;
-               ans=ans||helper(visited,adj,i);
-           }
-       }
-       return ans;
-    }
-    public boolean helper(boolean visited[],ArrayList<ArrayList<Integer>> adj,int source)
-    {
-        Queue<data> q=new LinkedList<>();
-        q.add(new data(source,-1));
-        while(!q.isEmpty())
+        boolean visited[]=new boolean[V];
+        for(int i=0;i<V;i++)
         {
-            data temp=q.remove();
-            for(int i=0;i<adj.get(temp.child).size();i++)
+            if(!visited[i]&&dfs(V,adj,visited,i,-1))
             {
-                if(!visited[adj.get(temp.child).get(i)])
-                {
-                    visited[adj.get(temp.child).get(i)]=true;
-                    q.add(new data(adj.get(temp.child).get(i),temp.child));
-                }
-                else
-                {
-                    if(adj.get(temp.child).get(i)!=temp.parent)
-                    {
-                        return true;
-                    }
-                }
+                return true;
+            }
+        }
+        return false;
+    }
+    public boolean dfs(int V,ArrayList<ArrayList<Integer>> adj,boolean visited[],int child,int parent)
+    {
+        visited[child]=true;
+        for(int i=0;i<adj.get(child).size();i++)
+        {
+            int node=adj.get(child).get(i);
+            if(visited[node]&&node!=parent)
+            {
+                return true;
+            }
+            else if(!visited[node]&&dfs(V,adj,visited,node,child))
+            {
+                return true;
             }
         }
         return false;
