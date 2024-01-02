2320767871345fbb1b829bcdbb47a781da59f117
Ujval Gupta <134779004+ug-is-og@users.noreply.github.com>
2024-01-02 14:12:24 +0530

    Time: 3 ms (25.30%), Space: 44.5 MB (31.52%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.java
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.java
@@ -71,18 +71,132 @@
 
 
 
+// class Solution {
+//     public boolean isBipartite(int[][] graph) {
+//         int visited[]=new int[graph.length];
+//         Queue<Integer> q=new LinkedList<>();
+//         Arrays.fill(visited,-1);
+//         for(int i=0;i<graph.length;i++)
+//         {
+//             if(visited[i]==-1)
+//             {
+//                 visited[i]=1;
+//                 q.add(i);
+//                 if(!bfs(graph,visited,q))
+//                 {
+//                     return false;
+//                 }
+//             }
+//         }
+//         return true;
+//     }
+//     public boolean bfs(int graph[][],int visited[],Queue<Integer> q)
+//     {
+//         while(!q.isEmpty())
+//         {
+//             int i=q.remove();
+//             int color=-1;
+//             if(visited[i]==1)
+//             color=0;
+//             else
+//             color=1;
+//             for(int j=0;j<graph[i].length;j++)
+//             {
+//                 if(visited[graph[i][j]]==color)
+//                 {
+//                     continue;
+//                 }
+//                 else if(visited[graph[i][j]]==-1)
+//                 {
+//                     visited[graph[i][j]]=color;
+//                     q.add(graph[i][j]);
+//                 }
+//                 else
+//                 {
+//                     return false;
+//                 }
+//             }
+//         }
+//         return true;
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let's solve using DFS
+
+// class Solution {
+//     public boolean isBipartite(int[][] graph) {
+//         int visited[]=new int[graph.length];
+//         Arrays.fill(visited,-1);
+//         for(int i=0;i<graph.length;i++)
+//         {
+//             if(visited[i]==-1&&!dfs(visited,i,graph,1))
+//             {
+//                 return false;
+//             }
+//         }
+//         return true;
+//     }
+//     public boolean dfs(int visited[],int i,int graph[][],int color)
+//     {
+//         System.out.println(i+" "+color);
+//         visited[i]=color;
+//         int nextColor=-1;
+//         if(color==0)
+//         {
+//             nextColor=1;
+//         }
+//         else
+//         {
+//             nextColor=0;
+//         }
+//         for(int k=0;k<graph[i].length;k++)
+//         {
+//             if(visited[graph[i][k]]==color)
+//             {
+//                 return false;
+//             }
+//             else if(visited[graph[i][k]]==-1&&!dfs(visited,graph[i][k],graph,nextColor))
+//             {
+//                 return false;
+//             }
+//         }
+//         return true;
+//     }
+// }
+
+
+
+
+
+
+// let's solve using BFS
+
+
 class Solution {
     public boolean isBipartite(int[][] graph) {
         int visited[]=new int[graph.length];
-        Queue<Integer> q=new LinkedList<>();
         Arrays.fill(visited,-1);
+        Queue<Pair<Integer,Integer>> q=new LinkedList<>();
         for(int i=0;i<graph.length;i++)
         {
             if(visited[i]==-1)
             {
                 visited[i]=1;
-                q.add(i);
-                if(!bfs(graph,visited,q))
+                q.add(new Pair<>(i,1));
+                if(!bfs(q,graph,visited))
                 {
                     return false;
                 }
@@ -90,30 +204,30 @@ class Solution {
         }
         return true;
     }
-    public boolean bfs(int graph[][],int visited[],Queue<Integer> q)
+    public boolean bfs(Queue<Pair<Integer,Integer>> q,int graph[][],int visited[])
     {
         while(!q.isEmpty())
         {
-            int i=q.remove();
-            int color=-1;
-            if(visited[i]==1)
-            color=0;
-            else
-            color=1;
-            for(int j=0;j<graph[i].length;j++)
+            Pair<Integer,Integer> temp=q.remove();
+            int nextColor=-1;
+            if(temp.getValue()==0)
             {
-                if(visited[graph[i][j]]==color)
-                {
-                    continue;
-                }
-                else if(visited[graph[i][j]]==-1)
-                {
-                    visited[graph[i][j]]=color;
-                    q.add(graph[i][j]);
-                }
-                else
+                nextColor=1;
+            }
+            else
+            {
+                nextColor=0;
+            }
+            for(int i=0;i<graph[temp.getKey()].length;i++)
+            {
+                if(visited[graph[temp.getKey()][i]]==temp.getValue())
                 {
                     return false;
+                }
+                else if(visited[graph[temp.getKey()][i]]==-1)
+                {
+                    visited[graph[temp.getKey()][i]]=nextColor;
+                    q.add(new Pair<>(graph[temp.getKey()][i],nextColor));
                 }
             }
         }
